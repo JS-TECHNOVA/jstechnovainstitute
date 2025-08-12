9297c5c5ce811811846ec7347105823cd953e94d
Shubham Chauhan <elskerelvish@outlook.com>
2025-08-12 07:47:11 +0530

    sitemaps

---
+++ institute/sitemaps.py
@@ -0,0 +1,14 @@
+from django.contrib import sitemaps
+from django.urls import reverse
+
+
+class StaticViewSitemap(sitemaps.Sitemap):
+    priority = 0.5
+    changefreq = "monthly"
+
+    def items(self):
+        return ["home", "about", "BookDemoClass","tutors"]
+
+    def location(self, item):
+        return reverse(item)
+
--- institute/urls.py
+++ institute/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path
 from . import views
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import StaticViewSitemap
+
+
+sitemaps = {
+    "static": StaticViewSitemap,
+}
 
 urlpatterns = [
     path("", views.Home.as_view(), name="home"),
     path("get-in-touch/", views.SubmitGetInTouchForm.as_view(), name="SubmitGetInTouchForm"), 
     path("about/", views.AboutView.as_view(), name="about"), 
     path("book-demo-class/", views.BookDemoClass.as_view(), name="BookDemoClass"), 
-    path("tutors/", views.Tutors.as_view(), name="tutors"), 
+    path("tutors/", views.Tutors.as_view(), name="tutors"),
+
+         path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
 ]
